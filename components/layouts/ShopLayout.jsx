d6097fd5867b58d51829fa2f76268b360cffcd37
heynar-max <heynar76@hotmail.com>
2023-07-19 21:33:21 -0500

    Layout  ShopLayout

---
+++ components/layouts/ShopLayout.jsx
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+
+
+
+export const ShopLayout = ({ children, title, pageDescription, imageFullUrl }) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+
+                <meta name='description' content={ pageDescription }/>
+
+                {/* para mostrar en las redes sociales cuando se comparte */}
+                <meta name='og:title' content={ title }/>
+                <meta name='og.description' content={ pageDescription }/>
+                {
+                    // && si tenemos muestre esta etiqueta
+                    imageFullUrl && (
+                        <meta name='og.image' content={ imageFullUrl }/>
+                    )
+                }
+
+            </Head>
+
+            <nav>
+                {/* TODO: Navbar */}
+            </nav>
+
+                {/* TODO: sidebar */}
+                
+            <main style={{
+                margin: '80px auto',
+                maxWidth: '1440px',
+                padding: '0px 30px'
+            }}>
+                {children}
+            </main>
+
+            {/* Footer */}
+            <footer>
+                {/* Todo: footer */}
+            </footer>
+        </>
+    )
+}
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-
-
+import { ShopLayout } from '@/components/layouts'
+import Typography from '@mui/material/Typography'
 
 
 export default function Home() {
   return (
-    <h1>Vanlia</h1>
+    <ShopLayout title={'Vanlia - Home'} pageDescription={'Encuentra los mejores productos de charcuteria'} imageFullUrl={''}>
+      <Typography variant="h1" color="h1">Charcuteria</Typography>
+      <Typography variant="h1" sx={{mb : 1}}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
