b0bb9c525caf7a922227644dab09e29340650ec8
heynar-max <heynar76@hotmail.com>
2023-07-19 21:27:44 -0500

    instalacion material UI y styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ lightTheme }>
+      <CssBaseline/>
+      <Component {...pageProps}/>
+    </ThemeProvider>
+  )
 }
