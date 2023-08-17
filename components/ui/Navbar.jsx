8387e7e7d58bf88cb24f7af36314aecbe3386fce
heynar-max <heynar76@hotmail.com>
2023-08-17 15:25:10 -0500

    Mostrar y ocultar texto de búsqueda

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -1,74 +1,124 @@
 import NextLink from 'next/link';
-import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
+import { AppBar, Badge, Box, Button, IconButton, Input, InputAdornment, Link, Toolbar, Typography } from '@mui/material';
 import Image from 'next/image';
-import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+import { ClearOutlined, SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
 import { useRouter } from 'next/router';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UiContext } from '@/context';
 
 export const Navbar = () => {
 
 
-    const { asPath } = useRouter();
+    const { asPath, push } = useRouter();
     const { toggleSideMenu } = useContext( UiContext );
 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+    const onSearchTerm = () => {
+        if( searchTerm.trim().length === 0 ) return;
+        push(`/search/${ searchTerm }`);
+    }
+
     return (
-<AppBar width='100px'>
-        <Toolbar >
-            <NextLink href='/' passHref legacyBehavior>
-                <Link display='flex' alignItems='center' sx={{ mt: .5}}>
-                    <Image src="/img/vanlia.png" alt='Logo' width='140' height='90' priority={true}/>
-                </Link>  
-            </NextLink>
+        <AppBar width='100px'>
+                <Toolbar >
+                    <NextLink href='/' passHref legacyBehavior>
+                        <Link display='flex' alignItems='center' sx={{ mt: .5}}>
+                            <Image src="/img/vanlia.png" alt='Logo' width='140' height='90' priority={true}/>
+                        </Link>  
+                    </NextLink>
 
-            {/* TODO flex */}
-            <Box flex={ 1 } />
+                    {/* TODO flex */}
+                    <Box flex={ 1 } />
 
-            <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-                <NextLink href='/category/salchichas' passHref legacyBehavior>
-                    <Link>
-                        <Button color={asPath === '/category/salchichas' ? 'secondary':'primary'} sx={{ mr: 1}}>SALCHICHAS</Button>
-                    </Link>
-                </NextLink>
-                <NextLink href='/category/chorizos' passHref legacyBehavior>
-                    <Link>
-                        <Button color={asPath === '/category/chorizos' ? 'secondary':'primary'} sx={{ mr: 1}}>CHORIZOS</Button>
-                    </Link>
-                </NextLink>
-                <NextLink href='/category/ahumados' passHref legacyBehavior>
-                    <Link>
-                        <Button color={asPath === '/category/ahumados' ? 'secondary':'primary'} sx={{ mr: 1}}>AHUMADOS</Button>
-                    </Link>
-                </NextLink>
-                <NextLink href='/category/quesos' passHref legacyBehavior>
-                    <Link>
-                        <Button color={asPath === '/category/quesos' ? 'secondary':'primary'} sx={{ mr: 1}}>QUESOS</Button>
-                    </Link>
-                </NextLink>
-            </Box>
+                    <Box sx={{ display: isSearchVisible ? 'none' :  { xs: 'none', md: 'block' } }}
+                        className="fadeIn">
+                        <NextLink href='/category/salchichas' passHref legacyBehavior>
+                            <Link>
+                                <Button color={asPath === '/category/salchichas' ? 'secondary':'primary'} sx={{ mr: 1}}>SALCHICHAS</Button>
+                            </Link>
+                        </NextLink>
+                        <NextLink href='/category/chorizos' passHref legacyBehavior>
+                            <Link>
+                                <Button color={asPath === '/category/chorizos' ? 'secondary':'primary'} sx={{ mr: 1}}>CHORIZOS</Button>
+                            </Link>
+                        </NextLink>
+                        <NextLink href='/category/ahumados' passHref legacyBehavior>
+                            <Link>
+                                <Button color={asPath === '/category/ahumados' ? 'secondary':'primary'} sx={{ mr: 1}}>AHUMADOS</Button>
+                            </Link>
+                        </NextLink>
+                        <NextLink href='/category/quesos' passHref legacyBehavior>
+                            <Link>
+                                <Button color={asPath === '/category/quesos' ? 'secondary':'primary'} sx={{ mr: 1}}>QUESOS</Button>
+                            </Link>
+                        </NextLink>
+                    </Box>
 
-            {/* TODO flex */}
-            <Box flex={ 1 } />
+                    {/* TODO flex */}
+                    <Box flex={ 1 } />
 
-            <IconButton>
-                <SearchOutlined />
-            </IconButton>
+                    {/* Pantallas pantallas grandes */}
+                    {
+                            isSearchVisible 
+                                ? (
+                                    <Input
+                                        sx={{ display: { xs: 'none', sm: 'flex' } }}
+                                        className='fadeIn'
+                                        autoFocus
+                                        value={ searchTerm }
+                                        onChange={ (e) => setSearchTerm( e.target.value ) }
+                                        onKeyPress={ (e) => e.key === 'Enter' ? onSearchTerm() : null }
+                                        type='text'
+                                        placeholder="Buscar..."
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    onClick={ () => setIsSearchVisible(false) }
+                                                >
+                                                    <ClearOutlined />
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                )
+                            : 
+                            (
+                                <IconButton 
+                                    onClick={ () => setIsSearchVisible(true) }
+                                    className="fadeIn"
+                                    sx={{ display: { xs: 'none', sm: 'flex' } }}
+                                >
+                                    <SearchOutlined />
+                                </IconButton>
+                            )
+                        }
 
-            <NextLink href="/cart" passHref legacyBehavior>
-                <Link>
-                    <IconButton>
-                        <Badge badgeContent={2} color="secondary">
-                            <ShoppingCartOutlined />
-                        </Badge>
-                    </IconButton>
-                </Link>
-            </NextLink>
+
+                        {/* Pantallas pequeñas */}
+                        <IconButton
+                            sx={{ display: { xs: 'flex', sm: 'none' } }}
+                            onClick={ toggleSideMenu }
+                        >
+                            <SearchOutlined />
+                        </IconButton>
+
+                    <NextLink href="/cart" passHref legacyBehavior>
+                        <Link>
+                            <IconButton>
+                                <Badge badgeContent={2} color="secondary">
+                                    <ShoppingCartOutlined />
+                                </Badge>
+                            </IconButton>
+                        </Link>
+                    </NextLink>
 
 
-            <Button onClick={ toggleSideMenu }>
-                Menú
-            </Button>
-        </Toolbar>
-</AppBar>
+                    <Button onClick={ toggleSideMenu }>
+                        Menú
+                    </Button>
+                </Toolbar>
+        </AppBar>
     )
 }
