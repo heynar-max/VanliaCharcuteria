c1a7d0620cd45c855fda8605f977671f3ad35535
heynar-max <heynar76@hotmail.com>
2023-08-30 16:18:48 -0500

    Modelo de usuario

---
+++ interfaces/user.js
@@ -0,0 +1,10 @@
+export const IUser = {
+    _id      : String,
+    name     : String,
+    email    : String,
+    password : String,
+    role     : String,
+
+    createdAt : String,
+    updatedAt : String,
+}
---
+++ models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.Schema({
+
+    name    : { type: String, required: true },
+    email   : { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: {
+            values: ['admin','client'],
+            message: '{VALUE} no es un role válido',
+            default: 'client',
+            required: true
+        }
+    }
+}, {
+    timestamps: true,
+})
+
+const User = mongoose.models.User || mongoose.model('User',userSchema);
+
+export default User;
+
+// si quiere un grupo de roles va
+// role: [{
+// }]
