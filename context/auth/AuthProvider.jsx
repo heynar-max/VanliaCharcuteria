7aa6a667080c6949145163cb61c263a4943161ab
heynar-max <heynar76@hotmail.com>
2023-09-16 09:20:08 -0500

    eliminar las cookies cuando se desloguea

--- context/auth/AuthProvider.jsx
+++ context/auth/AuthProvider.jsx
@@ -86,6 +86,15 @@ export const AuthProvider= ({ children }) => {
     const logout = () => {
         Cookies.remove('token');
         Cookies.remove('cart');
+        Cookies.remove('firstName');
+        Cookies.remove('lastName');
+        Cookies.remove('address');
+        Cookies.remove('address2');
+        Cookies.remove('zip');
+        Cookies.remove('city');
+        Cookies.remove('country');
+        Cookies.remove('phone');
+
         router.reload();
     }
 
