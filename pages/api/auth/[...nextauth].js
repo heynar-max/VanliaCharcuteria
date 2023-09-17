d143063c0aa9ebfb766c4151e1615bdb8ecd3a38
heynar-max <heynar76@hotmail.com>
2023-09-17 18:34:23 -0500

    NextAuth Callbacks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -36,7 +36,36 @@ export const authOptions = {
 
     callbacks: {
 
+        async jwt({ token, account, user }) {
+          // console.log({ token, account, user });
+    
+            if ( account ) {
+                token.accessToken = account.access_token;
         
+                switch( account.type ) {
+        
+                
+        
+                case 'credentials':
+                    token.user = user;
+                break;
+                }
+        
+            }
+        
+            return token;
+        },
+    
+    
+        async session({ session, token, user }){
+    
+            session.accessToken = token.accessToken;
+            session.user = token.user;
+        
+            return session;
+        }
+        
+    
     }
 }
 export default NextAuth(authOptions)
