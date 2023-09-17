e6971483f628f12e510139845bb419b95047e049
heynar-max <heynar76@hotmail.com>
2023-09-17 18:21:04 -0500

    Proveedor - credenciales

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,27 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from 'next-auth/providers/credentials';
 
 
 export const authOptions = {
   // Configure one or more authentication providers
     providers: [
+        Credentials({
+            name: 'Custom Login',
+            credentials: {
+                email: { label: 'Correo:', type: 'email', placeholder: 'correo@google.com'  },
+                password: { label: 'Contraseña:', type: 'password', placeholder: 'Contraseña'  },
+            },
+            async authorize(credentials) {
+                console.log({credentials})
+                return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
+    
+                // return await dbUsers.checkUserEmailPassword( credentials.email, credentials.password );
+    
+            }
+
+        }),
         GithubProvider({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET,
@@ -16,5 +32,11 @@ export const authOptions = {
         }),
         // ...add more providers here
     ],
+
+
+    callbacks: {
+
+        
+    }
 }
 export default NextAuth(authOptions)
