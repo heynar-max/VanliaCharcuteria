a2e4586cad70c8d914855c9107ab0108c539d900
heynar-max <heynar76@hotmail.com>
2023-10-08 19:51:53 -0500

    REST - GetProductos y listado

---
+++ pages/api/admin/products.js
@@ -0,0 +1,46 @@
+
+
+
+import { db } from '@/database';
+import { Product } from '@/models';
+
+
+
+
+export default function handler(req, res) {
+    
+        switch (req.method) {
+            case 'GET':
+                return getProducts( req, res );
+                
+            case 'PUT':
+                return updateProduct( req, res );
+
+            case 'POST':
+                return createProduct( req, res )
+                
+            default:
+                return res.status(400).json({ message: 'Bad request' });
+        }
+        
+    
+    }
+
+    const getProducts = async(req, res) => {
+        
+        await db.connect();
+
+        const products = await Product.find()
+            .sort({ title: 'asc' })
+            .lean();
+
+        await db.disconnect();
+
+    
+
+
+        res.status(200).json( products );
+
+    }
+
+
