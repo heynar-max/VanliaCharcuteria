4ee10ae5489a97298fb13bc17ef2ac549dff6500
heynar-max <heynar76@hotmail.com>
2023-10-18 21:08:39 -0500

    NO HACER - Almacenar imágenes el file system

--- pages/api/admin/upload.js
+++ pages/api/admin/upload.js
@@ -1,4 +1,5 @@
-import formidable from 'formidable';
+import { IncomingForm, File } from 'formidable';
+import fs from 'fs';
 
 export const config = {
     api: {
@@ -20,20 +21,43 @@ export default function handler(req, res) {
 }
 
 
+const saveFile = async( file ) => {
 
+    const data = fs.readFileSync( file.filepath );
+    fs.writeFileSync(`./public/${ file.originalFilename }`, data);
+    fs.unlinkSync( file.filepath ); // elimina
+    return;
+    
+
+}
 
 
 const parseFiles = async(req) => {
 
-    
+    return new Promise( (resolve, reject) => {
+
+        const form = new IncomingForm() ;
+        form.parse( req, async( err, fields, files ) => {
+            console.log({file: files.file});
+
+            if ( err ) {
+                return reject(err);
+            }
+
+            const filePath = await saveFile( files.file[0]  )
+            resolve(filePath);
+        })
+
+    }) 
 
 }
 
 
 const uploadFile = async(req, res) => {
     
+    await parseFiles(req);
     // const imageUrl = await parseFiles(req);
     
-    return res.status(200).json({ message: imageUrl });
+    return res.status(200).json({ message: 'imageUrl' });
 
 }
